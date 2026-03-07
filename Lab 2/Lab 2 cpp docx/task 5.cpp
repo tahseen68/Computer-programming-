bdb3fa590590b94b4f2dafd893400c6137bad98b
Tahseen Ullah <tahsinnimat@gmail.com>
2026-03-07 22:28:54 +0500

    Add files via upload

---
+++ Lab 2/Lab 2 cpp docx/task 5.cpp
@@ -0,0 +1,24 @@
+
+//Sequential 
+//print your name 10 times 
+
+#include <iostream>// This line lets us use input cin and output cout in the program
+using namespace std;// Use the standard namespace to avoid std :: before  every cout
+
+int main()
+	{
+	
+	
+	cout<<"TAHSEEN ULLAH"<<endl; // Print my name 10 times and using endl with my name will end one line and creats a new line in the output
+	cout<<"TAHSEEN ULLAH"<<endl;// Print my name 10 times and using endl with my name will end one line and creats a new line in the output
+	cout<<"TAHSEEN ULLAH"<<endl;// Print my name 10 times and using endl with my name will end one line and creats a new line in the output
+	cout<<"TAHSEEN ULLAH"<<endl; // Print my name 10 times and using endl with my name will end one line and creats a new line in the output
+	cout<<"TAHSEEN ULLAH"<<endl;
+	cout<<"TAHSEEN ULLAH"<<endl;// Print my name 10 times and using endl with my name will end one line and creats a new line in the output
+	cout<<"TAHSEEN ULLAH"<<endl;
+	cout<<"TAHSEEN ULLAH"<<endl;
+	cout<<"TAHSEEN ULLAH"<<endl;// Print my name 10 times and using endl with my name will end one line and creats a new line in the output
+	cout<<"TAHSEEN ULLAH"<<endl;	
+		
+	return 0;}// program end successfuly
+
