21d054ede331019e50f1ae68b0a0229a4f67f79b
Tahseen Ullah <tahsinnimat@gmail.com>
2026-05-17 17:31:21 +0500

    Add files via upload

---
+++ Lab 9/Lab work/Lab_09 LabWork_01 (1).cpp
@@ -0,0 +1,22 @@
+// Name: tahseen ullah
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0735
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+// Function declaration
+void greet()
+{
+    // Display greeting message
+    cout << "Welcome to C++ Programming!" << endl;
+}
+
+int main()
+{
+    // Function call
+    greet();
+
+    return 0;
+}
---
+++ Lab 9/Lab work/Lab_09 LabWork_02 (1).cpp
@@ -0,0 +1,22 @@
+// Name: tahseen ullah
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0735
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+// Function declaration
+void sayHello()
+{
+    // Display message
+    cout << "Hello, World!" << endl;
+}
+
+int main()
+{
+    // Function call
+    sayHello();
+
+    return 0;
+}
