9c0ec1260e44a7416686284cccacde8f2251b7b1
Tahseen Ullah <tahsinnimat@gmail.com>
2026-05-17 16:37:19 +0500

    Add files via upload

---
+++ Lab 6/Lab work/Lab 6 lab work1.cpp
@@ -0,0 +1,18 @@
+// Name: Tahseen ullah 
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0735
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1;   // starting point
+
+    while (i <= 10) {
+        cout << i << endl;  // print number on new line
+        i++;                // move to next number
+    }
+
+    return 0;
+}
